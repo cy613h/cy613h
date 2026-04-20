637dbb75ecc63e3a736bd7b9adda2d815e181bfb
cy613h <cy613h@users.noreply.github.com>
2026-04-20 11:36:54 +0300

    image source

--- cy613h/urls.py
+++ cy613h/urls.py
@@ -25,5 +25,9 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
